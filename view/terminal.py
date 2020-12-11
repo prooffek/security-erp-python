1127d41c8445ad18dc0e4e917fef81f5bf9af466
EwelinaStasiak <ewelina.stasiak5@gmail.com>
2020-12-11 10:39:17 +0100

    change clear terminal

--- view/terminal.py
+++ view/terminal.py
@@ -14,7 +14,7 @@ def print_menu(title, list_options):
         title (str): the title of the menu (first row)
         list_options (list): list of the menu options (listed starting from 1, 0th element goes to the end)
     """
-    os.system("cls | clear")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
     print(f"{title}:\n")
 
